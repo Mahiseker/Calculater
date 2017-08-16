83c3fddeb512319d794ef75210dd31d7200cf178
Mahiseker Polat <sekermahipolat@hotmail.com>
2017-08-16 19:58:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mahiseker/calculater/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mahiseker.calculater;
+
+import android.renderscript.Double2;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView screen;
+    private String str1,str2,str3,result,str,sign;
+
+    private double a,b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        screen = (TextView) findViewById(R.id.textview);
+        str = "";
+    }
+    public void onClick(View v){
+        Button button=(Button)v;
+        str +=button.getText().toString();
+        screen.setText(str);
+        a= Double.parseDouble(str);
+    }
+    public void onClickSigns(View v){
+        Button button =(Button) v;
+        sign=button.getText().toString();
+        screen.setText(sign);
+        str="";
+    }
+    public void calculate(View v){
+        Button button=(Button) v;
+        str2=screen.getText().toString();
+        b=Double.parseDouble(str2);
+        if (sign.equals("+")) {
+            result = a + b + "";
+        }
+        else if (sign.equals("-")){
+            result=a-b+"";
+        }
+        else if (sign.equals("*")){
+            result=a*b+"";
+        }
+        else if (sign.equals("/")){
+            result=a/b+"";
+        }
+        else{
+            screen.setText("k");
+        }
+        screen.setText(result);
+
+
+    }
+}
